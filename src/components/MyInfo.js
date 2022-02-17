344466c184061f426822c72e79804c8d9180af24
Антон Швец <a.shvec@santehnika-tut.ru>
2022-02-17 13:59:08 +0300

    Внесены пробные правки

---
+++ src/components/MyInfo.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./MyInfo.scss"
+
+function MyInfo() {
+
+
+
+    return (
+<div>
+    <h1 className='firstName'> 
+Антон
+    </h1>
+    <p>
+        Это я 
+    </p>
+    <ul>
+        <ol>1</ol>
+        <ol>2</ol>
+        <ol>3</ol>
+    </ul>
+</div>
+
+    )
+}
+
+export default MyInfo;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MyInfo from './components/MyInfo';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MyInfo />
   </React.StrictMode>,
   document.getElementById('root')
 );
